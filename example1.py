1c318e1c64e0a52e6d8d39fa62ad1a3ebd902231
bp15046 <bp15046@shibaura-it.ac.jp>
2018-11-28 17:17:09 +0900

    fix sample code

--- example1.py
+++ example1.py
@@ -1,7 +1,7 @@
 from vlamp.vlamp import Lamp
 import time
-	
-    
+
+
 lamp = Lamp("172.29.156.35", 1883)
 lamp.enable()
 
@@ -9,4 +9,4 @@ lamp.on()
 time.sleep(3)
 lamp.off()
 
-lam.disable()
+lamp.disable()
